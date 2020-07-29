b0553a6b9dd69e0579c1b810cf693c4c59d49965
irtep <rasanen.petri@gmail.com>
2020-07-29 22:04:09 +0300

    update

---
+++ public/script.js
@@ -0,0 +1,94 @@
+import { addNewEntry, copyToClipboardMsg } from './functions.js';
+const allDivs = document.getElementById('container');
+const passInput = document.getElementById('passInput');
+const passAsker = document.getElementById('passAsker');
+const infoScreen = document.getElementById('infoScreen');
+const downLeft = document.getElementById('downLeft');
+const downRight = document.getElementById('downRight');
+const checkingPass = document.getElementById('passInput').addEventListener("change", checkPass);
+const sendNewEntry = document.getElementById('sendNew').addEventListener('click', addNewEntry);
+const haste1Listen = document.getElementById('haste1').addEventListener('click', copyHaste);
+let allData = null;
+/*
+function copyHaste(elem) {
+  const targetDiv = document.getElementById(elem.target.id);
+  // copy to clipboard
+  copyToClipboardMsg(targetDiv, "msg");
+  window.scrollTo(0, 0);
+}
+// this shows data of the clicked information
+function showData(clickedElement) {
+  // find the data:
+  allData.forEach( (dataEntry, idx) => {
+    if (dataEntry._id === clickedElement.target.id) {
+      // add data
+      downRight.innerHTML = allData[idx].response;
+      // copy to clipboard
+      copyToClipboardMsg(downRight, "msg");
+      window.scrollTo(0, 0);
+    }
+  });
+}
+*/
+function checkPass(pass) {
+  // correct password length
+  if (pass.target.value.length === 7) {
+    const passu = pass.target.value;
+    const http = new XMLHttpRequest();
+    const url = '/showAll';
+    let params = 'MSG=' + passu;
+
+    http.open('POST', url, true);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.onreadystatechange = () => {
+      if (http.readyState == 4 && http.status == 200) {
+        const records = JSON.parse(http.responseText);
+        //const records = http.responseText;
+        console.log('got records', records);
+        if (records === 'wrong password') {
+          infoScreen.innerHTML = 'wrong password!';
+        } else {
+          // correct pw
+          infoScreen.innerHTML = '';
+          passAsker.classList.add('invis');
+          allDivs.classList.remove('invis');
+          allData = records;
+          allData.forEach( data => {
+            //data.question
+            // add question to page
+            downLeft.innerHTML += `<p id= ${data._id} class= "clickable">${data.question}</p>`;
+            // add event listener to this
+            const elements = document.getElementsByClassName('clickable');
+            for (var i = 0; i < elements.length; i++) {
+              elements[i].addEventListener('click', showData, false);
+            }
+            // haste buttons... maybe temporary "dumb" solution, but gotta do more dynamic someday...
+            const haste1Listen = document.getElementById('haste1').addEventListener('click', copyHaste);
+            const haste2Listen = document.getElementById('haste2').addEventListener('click', copyHaste);
+            const haste3Listen = document.getElementById('haste3').addEventListener('click', copyHaste);
+            const haste4Listen = document.getElementById('haste4').addEventListener('click', copyHaste);
+          });
+        }
+      }
+    }
+    http.send(params);
+  } else {
+    infoScreen.innerHTML = 'wrong password!';
+  }
+}
+window.onload = (()=> {
+  const allDivs = document.getElementById('container');
+  allDivs.classList.add('invis');
+
+  // even listeners for hastes
+  /*
+  const allHastes = document.getElementsByClassName('hastes')
+  console.log('all hastes', allHastes);
+  for (let i = 0; i < allHastes.length; i++) {
+    allHastes[i].addEventListener('click', copyHaste);
+  }
+  */
+  //allHastes.forEach( (aH, idx) => {
+  //  aH.addEventListener('click', copyHaste);
+  //});
+});
---
+++ server.js
@@ -0,0 +1,83 @@
+"use strict";
+const process = require ('./config.js');
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const pasw = process.env.SECRET2;
+app.use(express.static('public'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+/*
+// database access:
+const mongoose = require('mongoose');
+const mongoDB = process.env.SECRET1; // admin
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+const Schema = mongoose.Schema;
+const entrySchema = new Schema( {
+  question: {
+    type: String
+  },
+  response: {
+    type: String
+  }
+});
+const entryModel = mongoose.model('ot2model', entrySchema );
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+*/
+// GET handler
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+  console.log('got get');
+});
+// POST handlers
+// show all entries
+/*
+app.post('/showAll', (request, response) => {
+  const requestPass = request.body.MSG;
+  const newDbConnect = new Promise( (resolve, reject) => {
+    entryModel.find((err, results) => {
+      if (results !== null) {
+        resolve(results);
+      }
+    });
+  });
+  newDbConnect.then( (results) => {
+    let responding = null;
+    if (pasw === requestPass) {
+      responding = JSON.stringify(results);
+    } else {
+      responding = 'wrong password';
+    }
+    const sending = responding;
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(sending);
+  });
+
+});
+*/
+// add new entry:
+/*
+app.post('/addNew', (request, response) => {
+  const newEntryComing = JSON.parse(request.body.MSG);
+  //console.log('new entry: ', newEntryComing);
+  const theQuery = { name: 'ot2'};
+  const newEntry = entryModel({
+    question: newEntryComing.question,
+    response: newEntryComing.response
+  });
+  newEntry.save( (err, entry1) => {
+    if (err) return console.error(err);
+    console.log("saved to entries collection.");
+  });
+  const sending = JSON.stringify('Database updated successfully!');
+  console.log("responding with data ");
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end(sending);
+
+});
+*/
+const listener = app.listen(5599, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
